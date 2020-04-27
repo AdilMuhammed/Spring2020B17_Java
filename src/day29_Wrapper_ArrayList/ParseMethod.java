1718126bf2b324220529677e5b47275da28430a8
AdilMuhammed <adilabdulrahman83@gmail.com>
2020-04-27 11:50:41 -0400

    Day29 Class

--- src/day29_Wrapper_ArrayList/ParseMethod.java
+++ src/day29_Wrapper_ArrayList/ParseMethod.java
@@ -27,7 +27,29 @@ public class ParseMethod {
         String str2 = "10.5";
 
         float f1 = Float.parseFloat(str2);     // float 10.5
-        System.out.println(f1);
+        System.out.println(f1+1);
+
+        double f2 = Double.parseDouble(str2);
+        System.out.println(f2+3);
+
+
+        String str3 = "2147483647";
+        long num1 = Long.parseLong(str3);    // 2147483647L
+        System.out.println(num1+2);
+
+        Long num2 = Long.parseLong(str3);
+        System.out.println(num2+2);
+
+
+        String result1 = "TrUE";
+        boolean r1 = Boolean.parseBoolean(result1);     // boolean
+        System.out.println(!r1);
+
+
+        String result2 = "faLSE";
+        boolean r2 = Boolean.parseBoolean(result2);     // boolean
+        System.out.println(!r2);
+
 
     }
 
