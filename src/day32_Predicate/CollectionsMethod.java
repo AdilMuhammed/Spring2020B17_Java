b15dcdcd5f4f68d3b765860f75bd693e6f919a41
AdilMuhammed <adilabdulrahman83@gmail.com>
2020-05-01 15:12:21 -0400

    Day32 Class

--- src/day32_Predicate/CollectionsMethod.java
+++ src/day32_Predicate/CollectionsMethod.java
@@ -17,6 +17,71 @@ public class CollectionsMethod {
         System.out.println(min);
 
 
+         /*
+            1. write a program that can return the second maximum number from Arraylist
+                    Ex: {1,2,3,4,5,6,7,8,8};
+                            output: 7
+            2. write a program that can return the second minimum number from Arraylist
+                Ex: {1,1,2,3,4,5,6,7,8,8};
+                   output: 2
+         */
+
+         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,1,2,3,4,4));
+            // second max: 3
+            // second min: 2
+
+        Integer maxNum = Collections.max(numbers);  // 4
+
+        numbers.removeAll(Arrays.asList(maxNum));
+        System.out.println(numbers);
+
+        int secondMax = Collections.max(numbers);
+        System.out.println(secondMax);
+
+
+        System.out.println("=============================================");
+
+
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1,1,2,3,4,5,6,7,8));
+
+        Integer minNum = Collections.min(list2);
+
+        list2.removeAll(Arrays.asList(minNum));
+        System.out.println(list2);
+
+        int secondMin = Collections.min(list2);
+        System.out.println(secondMin);
+
+
+        System.out.println("=============================================");
+
+        ArrayList<Integer> list3 = new ArrayList<>();
+            list3.addAll(Arrays.asList(1,2,3,4,5));     // {1,2,3,4,5}
+        //                                                  0       4
+        System.out.println(list3);
+
+        Collections.swap(list3,0,4);
+        System.out.println(list3);
+
+
+        System.out.println("=============================================");
+
+        ArrayList<String> names = new ArrayList<>();
+        names.addAll(Arrays.asList("Omer", "Omer", "Mary", "Sha", "Fatih", "Omer"));
+
+        /*
+        names.set(0,"Irina");
+        names.set(1,"Irina");
+        names.set(5,"Irina");
+         */
+
+
+        Collections.replaceAll( names,"Omer", "Irina");
+
+        System.out.println(names);
+
+
+
 
 
     }
